32e60c26299e2dcad6a5411f43abddb38df76ac4
s_hiroshima91 <103233715+s-hiroshima91@users.noreply.github.com>
2022-08-22 19:30:28 +0900

    Add files via upload

--- src/ppu.cpp
+++ src/ppu.cpp
@@ -12,8 +12,8 @@ inline int C2I(char value){
 /*場所からパレットを紐付ける関数*/
 inline char PaletteNum(char value, int x, int y){
 	int num;
-	/*縦32lineで1単位なので*/
-	/*下の二行で32で割って8かける操作*/
+	/*縦32lineで1単位なので
+	下の二行で32で割って8かける操作*/
 	num = y >> 2;
 	num &= 0b0100;
 	
@@ -30,7 +30,7 @@ inline char PaletteNum(char value, int x, int y){
 /*場所からパターンテーブルを決定するクラス*/
 int Ppu::PrePattern(int x, int y, int name, int line, bool flg){
 	int addr;
-	addr = C2I((ppuTable[x + y +name]));
+	addr = C2I((ppuTable[x + y + name]));
 	addr <<= 4;
 	addr |= ((static_cast<int>(flg)) << 12); 	
 	addr += (line & 0x07);
@@ -47,6 +47,7 @@ char Ppu::PreConvColor(int x, int y, int name, int line){
 	return value;
 }
 
+/*インストラクタ*/
 Ppu::Ppu(char *romDate, char header6, IOPort *ioP){
 	
 	rom = romDate;
@@ -57,6 +58,7 @@ Ppu::Ppu(char *romDate, char header6, IOPort *ioP){
 	ioPort->ppuIO[0x0002] = 0b10000000;
 }
 
+/*メモリマップ*/
 char *Ppu::MemoryMap(unsigned short addr){
 	char *pointer;
 	if (addr < 0x2000){
@@ -70,7 +72,7 @@ char *Ppu::MemoryMap(unsigned short addr){
 		addr %= 0x0020;
 		pointer = &ppuPalette[addr];
 	}else{
-		std::cout << "ppuerror 0x" << addr << std::endl;
+		std::cout << "ppuerror 0x" << std::hex << addr << std::endl;
 	}
 	return pointer;
 }
@@ -86,16 +88,21 @@ void Ppu::CreateImg(char *bg){
 	int x;
 
 	counter = 0;
+	/*背景のパレット選択*/
 	bFlg = CheckBit(ctrRegister1, 4);
-
+	
+	/*ネームテーブルの2ビット目を取得*/
 	nameAddrY = (static_cast<int>(ctrRegister1) & 0b10) << 10;
 	
+	/*Yオフセット*/
 	currentLine = lineCounter + C2I(scroll[1]);
 	
+	/*Y方向のネームテーブルを選択*/
 	if (currentLine >= 240){
 		nameAddrY ^= 0b100000000000;
 		currentLine -= 240;
 	}
+	/*垂直ミラーの場合の処理*/
 	if (vMirror){
 		nameAddrY = 0;
 	}
@@ -106,16 +113,20 @@ void Ppu::CreateImg(char *bg){
 	//line / 32 *8
 	line2 = ((currentLine >> 2) & 0x38) + 0x03c0;
 	
+/*スプライト0の処理。未実装
 	if (C2I(spriteTable[0]) == lineCounter + 1){
 		ioPort->ppuIO[0x0002] |= 0b01000000;
 	}else{
 		ioPort->ppuIO[0x0002] &= 0b10111111;
-	}
+	}*/
 	
+	/*ネームテーブルの1ビット目を取得*/
 	nameAddrX = (static_cast<int>(ctrRegister1) & 0b01) << 10;
 	
+	/*ネームテーブルの選択*/
 	nameAddr = nameAddrX + nameAddrY;
 	
+	/*中途半端に始まる最初のブロックの処理*/
 	x = C2I(scroll[0]) >> 3;
 	j = C2I(scroll[0]) & 0b0111;
 	
@@ -138,12 +149,14 @@ void Ppu::CreateImg(char *bg){
 	}
 	x += 1;
 	
+	/*2ブロック目以降の処理*/
 	for (i = 0; i < 32; ++i){
+		/*X方向のネームテーブルを選択*/
 		if(x >= 32){
 			x -= 32;
 			nameAddrX ^= 0b010000000000;
 		}
-		
+		/*水平ミラーの場合の処理*/
 		if (! vMirror){
 			nameAddrX = 0;
 		}
@@ -171,6 +184,7 @@ void Ppu::CreateImg(char *bg){
 		}
 		++x;
 	}
+	
 	/*必要に応じてスプライトデータを上書き*/
 	for (int k =0; k < 8; ++k){
 		if (spriteBuffer[k].counter < 8 ){
@@ -197,6 +211,7 @@ void Ppu::CreateImg(char *bg){
 	lineCounter += 1;
 }
 
+/*スプライトイメージをバッファに生成*/
 void Ppu::SpriteImg(int x, int y){
 	int flg;
 	int sflg;
@@ -205,36 +220,49 @@ void Ppu::SpriteImg(int x, int y){
 	char table1, table2, temp, colorPalette;
 	bool hRev, vRev;
 	
-	
+	/*スプライトのデープル選択*/
 	sflg = static_cast<int>(CheckBit(ctrRegister1, 3)) << 12;
 	y -= 1;
 	x <<= 3;
-
+	
 	for (int i = 0; i < 2; ++i){
+		
+		/*ライン上にスプライトが見つかった場合*/
+		if (C2I(spriteTable[x]) == y){
+			
+			/*スプライトのオーバフローを確認*/
 		if (spriteBuffer[bufferNum].counter != 8){
 			ioPort->ppuIO[0x0002] |= 0b00100000;
 			return;
 		}
 
 		ioPort->ppuIO[0x0002] &= 0b11011111;
-		
-		if (C2I(spriteTable[x]) == y){
+			
+			/*0ライン目にセット*/
 			spriteBuffer[bufferNum].counter = 0;
 			x += 1;
 			
+			/*スプライトパターンのアドレスを取得*/
 			addr = spriteTable[x] << 4;
 			addr += sflg;
+			
 			x += 1;
 			
+			/*背景優先かどうかを判定*/
 			spriteBuffer[bufferNum].bgFlg = CheckBit(spriteTable[x], 5);
+			/*水平反転かどうかを判定*/
 			hRev = CheckBit(spriteTable[x], 6);
+			/*垂直反転かどうかを判定*/
 			vRev = CheckBit(spriteTable[x], 7);
+			/*カラーパレットの上位2ビットを取得*/
 			colorPalette = ((spriteTable[x] << 2) & 0x0f) | 0x10 ;
 			x += 1;
 			
+			/*スプライトのx座標の位置*/
 			spriteBuffer[bufferNum].x = C2I(spriteTable[x]);
 			x += 1;
 			
+			/*反転の処理*/
 			if (hRev){
 				hInit = 0;
 				hChange = 1;
@@ -250,6 +278,7 @@ void Ppu::SpriteImg(int x, int y){
 				vChange = 1;
 			}
 			
+			/*スプライトデータをバッファに展開*/
 			for (int j = 0; j < 8; ++j){
 				table1 = rom[vPos];
 				table2 = rom[vPos + 8];
