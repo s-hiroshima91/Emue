5bc694f64c484d82621f6b0f5b5680806875d9bb
s_hiroshima91 <103233715+s-hiroshima91@users.noreply.github.com>
2023-11-04 13:21:39 +0900

    Add files via upload

--- src/cpubase.cpp
+++ src/cpubase.cpp
@@ -1,428 +1,310 @@
-#include <iostream>
 #include "cpu.hpp"
-#include "ioport.hpp"
-#include "mapper.hpp"
-#include "common.hpp"
+#include "cpuinline.hpp"
+#include "cartridge.hpp"
+#include <iostream>
 
-//以下、cpuの操作
 
-/*対象のレジスタの値をレジスタに入力*/
-void Cpu::TrO(uint8_t *wRegister, uint8_t operand){
-	bool status;
-	*wRegister = operand;
-	status = CheckZero(*wRegister);
-	StatusRegi(zFlg, status);
-	status = CheckBit(*wRegister, 7);
-	StatusRegi(nFlg, status);
+char Cpu::Imm(){
+	char value;
+	value = *MemoryMap(registerPC);
+	++registerPC;
+	return value;
 }
 
-/*対象の値をレジスタに入力*/
-void Cpu::LdO(uint8_t *wRegister, uint16_t addr){
-	uint8_t value;
-	value = *MemoryMap(addr);
-	TrO(wRegister, value);
+ushort Cpu::Abs(){
+	char value;
+	value = Imm();
+	return Byte1to2(Imm(), value);
 }
 
-/*レジスタの値を対象に入力*/
-int Cpu::StO(uint16_t wAddr, uint8_t rRegister){
-	uint8_t *ptr;
-	if (wAddr < 0x2000){
-		ptr = MemoryMap(wAddr);
-		*ptr = rRegister;
-	}else if (wAddr < 0x4020){
-		ioPort->IOWrite(rRegister, wAddr);
-		if (wAddr == 0x4014){
-			return DMA();
-		}
-		
-	}else if (wAddr < 0x8000){		
-		ptr = MemoryMap(wAddr);
-		*ptr = rRegister;
+void Cpu::Adc(char value){
+	bool carry;
+	carry = CheckBit(registerF, cFlg);	
+	ushort result;
+	registerF &= 0x0f;
+	if (carry){
+		registerF |= HalfCarry2(registerA, value);
+		result = C2US(value) + 1;
 	}else{
-		mapper->MapperCall(rRegister, wAddr);
+		registerF |= HalfCarry(registerA, value);
+		result = C2US(value);
 	}
-	return 0;
-}
-
-/*対象の値をレジスタAに加算する*/
-void Cpu::AdcBase(uint8_t *wRegister, uint8_t operand){
-	uint16_t calc;
-	bool flgA, flgB, cStatu, zStatu ,vStatu, nStatu;
-	/*キャリーオーバを防ぐためshortで計算する*/
-	calc = static_cast<uint16_t>(*wRegister) + static_cast<uint16_t>(operand);
-	cStatu = CheckBit(registerP, cFlg);
-	calc += static_cast<uint16_t>(cStatu);
-	cStatu = static_cast<bool>(calc >> 8);
-	flgA = CheckBit(*wRegister, 7);
-	flgB = CheckBit(operand, 7);
-	*wRegister = static_cast<uint8_t>(calc & 0x00ff);
-	nStatu = CheckBit(*wRegister, 7);
-	vStatu = flgA & flgB & (! nStatu);
-	vStatu |= ((! flgA) & (! flgB) & nStatu);
-	zStatu = CheckZero(*wRegister);
-	StatusRegi(cFlg, cStatu);
-	StatusRegi(zFlg, zStatu);
-	StatusRegi(vFlg, vStatu);
-	StatusRegi(nFlg, nStatu);
-}
-
-void Cpu::Adc(uint8_t *wRegister, uint16_t addr){
-	uint8_t value;
-	value = *MemoryMap(addr);
-	AdcBase(wRegister, value);
-}
-
-/*対象の値をレジスタAから引く*/
-void Cpu::Sbc(uint8_t *wRegister, uint16_t addr){
-	uint8_t value;
-	/*わたす値を2の補数にすることで足し算と共通の計算とする。*/
-	value = *MemoryMap(addr);
-	value ^= 0xff; //2の補数計算
-	//operand++;  ここで1を足さないことで足し算とキャリーフラグの扱いを共通化
-	AdcBase(wRegister, value);
-}
-
-/*対象の値とレジスタAのAnd*/
-void Cpu::And(uint16_t addr){
-	bool status; 
-	registerA &= *MemoryMap(addr);
-	status = CheckZero(registerA);
-	StatusRegi(zFlg, status);
-	status = CheckBit(registerA, 7);
-	StatusRegi(nFlg, status);
-}
-
-/*対象の値とレジスタAのOr*/
-void Cpu::Ora(uint16_t addr){
-	bool status;
-	registerA |= *MemoryMap(addr);
-	status = CheckZero(registerA);
-	StatusRegi(zFlg, status);
-	status = CheckBit(registerA, 7);
-	StatusRegi(nFlg, status);
-}
-
-/*対象の値とレジスタAのXor*/
-void Cpu::Eor(uint16_t addr){
-	bool status;
-	registerA ^= *MemoryMap(addr);
-	status = CheckZero(registerA);
-	StatusRegi(zFlg, status);
-	status = CheckBit(registerA, 7);
-	StatusRegi(nFlg, status);
-}
-
-/*対象の値を左シフト*/
-void Cpu::AslA(uint8_t *operand){
-	bool status;
-	status = CheckBit(*operand, 7);
-	StatusRegi(cFlg, status);
-	status = CheckBit(*operand, 6);
-	StatusRegi(nFlg, status);
-	*operand <<= 1;
-	status = CheckZero(*operand);
-	StatusRegi(zFlg, status);
-}
-
-/*対象の値を左シフト*/
-void Cpu::Asl(uint16_t addr){
-	uint8_t *ptr;
-	ptr = MemoryMap(addr);
-	AslA(ptr);
-}
-
-/*対象の値を右シフト*/
-void Cpu::LsrA(uint8_t *operand){
-	bool status;
-	status = CheckBit(*operand, 0);
-	StatusRegi(cFlg, status);
-	StatusRegi(nFlg, false);
-	*operand >>= 1;
-	/*最上位bitに0を入力*/
-	*operand &= 0b01111111;
-	status = CheckZero(*operand);
-	StatusRegi(zFlg, status);
-}
-
-/*対象の値を右シフト*/
-void Cpu::Lsr(uint16_t addr){
-	uint8_t *ptr;
-	ptr = MemoryMap(addr);
-	LsrA(ptr);
-}
-
-/*対象の値をキャリー含めて左シフト*/
-void Cpu::RolA(uint8_t *operand){
-	bool status;
-	uint8_t  temp;
-	status = CheckBit(registerP, cFlg);
-	temp = static_cast<uint8_t>(status);
-	status = CheckBit(*operand, 7);
-	StatusRegi(cFlg, status);
-	status = CheckBit(*operand, 6);
-	StatusRegi(nFlg, status);
-	*operand <<= 1;
-	*operand += temp;
-	status = CheckZero(*operand);
-	StatusRegi(zFlg, status);
-}
-
-/*対象の値をキャリー含めて左シフト*/
-void Cpu::Rol(uint16_t addr){
-	uint8_t *ptr;
-	ptr = MemoryMap(addr);
-	RolA(ptr);
-}
-
-/*対象の値をキャリー含めて右シフト*/
-void Cpu::RorA(uint8_t *operand){
-	bool status;
-	uint8_t temp;
-	status = CheckBit(registerP, cFlg);
-	temp = static_cast<uint8_t>(status);
-	temp <<= 7;
-	StatusRegi(nFlg, status);
-	status = CheckBit(*operand, 0);
-	StatusRegi(cFlg, status);
-	*operand >>= 1;
-	*operand &= 0b01111111;
-	*operand += temp;
-	status = CheckZero(*operand);
-	StatusRegi(zFlg, status);
-}
-
-/*対象の値をキャリー含めて右シフト*/
-void Cpu::Ror(uint16_t addr){
-	uint8_t *ptr;
-	ptr = MemoryMap(addr);
-	RorA(ptr);
-}
-
-/*対象のbitを確認*/
-void Cpu::Bit(uint16_t addr){
-	bool status;
-	uint8_t value;
-	value = *MemoryMap(addr);
-	status = CheckBit(value, 7);
-	StatusRegi(nFlg, status);
-	status = CheckBit(value, 6);
-	StatusRegi(vFlg, status);
-	value &= registerA;
-	status = CheckZero(value);
-	StatusRegi(zFlg, status);
-}
-
-/*対象の値と対象のレジスタの値を比較*/
-void Cpu::CmO(uint8_t rRegister, uint16_t rMemory){
-	uint8_t temp;
-	bool Vtemp;
-	temp = rRegister; //レジスタの値を書き換えない
-	Vtemp = CheckBit(registerP, vFlg);
-	StatusRegi(cFlg, true); //ボローなし引き算のためキャリーをセット
-	Sbc(&temp, rMemory);
-	StatusRegi(vFlg, Vtemp);
-}
-
-/*対象の値をインクリメント*/
-void Cpu::InO(uint8_t *addr){
-	bool status;
-	*addr += 1;
-	status = CheckZero(*addr);
-	StatusRegi(zFlg, status);
-	status = CheckBit(*addr, 7);
-	StatusRegi(nFlg, status);
-}
-
-/*対象の値をインクリメント*/
-void Cpu::Inc(uint16_t addr){
-	uint8_t *ptr;
-	ptr = MemoryMap(addr);
-	InO(ptr);
-}
-
-/*対象の値をデクリメント*/
-void Cpu::DeO(uint8_t *addr){
-	bool status;
-	*addr -= 1;
-	status = CheckBit(*addr, 7);
-	StatusRegi(nFlg, status);
-	status = CheckZero(*addr);
-	StatusRegi(zFlg, status);
-}
-
-/*対象の値をデクリメント*/
-void Cpu::Dec(uint16_t addr){
-	uint8_t *ptr;
-	ptr = MemoryMap(addr);
-	DeO(ptr);
-}
-
-/*レジスタの値をスタック*/
-void Cpu::PhO(uint8_t rRegister){
-	uint16_t addr;
-	uint8_t *ptr;
-	/*スタックのアドレスに変換*/
-	addr = static_cast<uint16_t>(registerS) | 0x0100;
-	ptr = MemoryMap(addr);
-	*ptr = rRegister;
-	registerS -= 1; //スタックポインタをデクリメント
-}
-
-/*スタックをレジスタの値に入力*/
-void Cpu::PlO(uint8_t *wRegister){
-	uint16_t addr;
-	bool status;
-	uint8_t value;
-	//スタックポインタをインクリメント
-	registerS += 1;
-	addr = static_cast<uint16_t>(registerS) | 0x0100;
-	value = *MemoryMap(addr);
-	status = CheckBit(value, 7);
-	StatusRegi(nFlg,status);
-	status = CheckZero(value);
-	StatusRegi(zFlg, status);
-	*wRegister = value;
-}
-
-/*ジャンプ*/
-void Cpu::Jmp(uint16_t addr){
-	registerPC = addr;
-}
-
-/*サブルーチン呼び出し*/
-void Cpu::Jsr(uint16_t addr){
-	uint8_t value;
-	registerPC -= 1;
-	/*プログラムカウンタの値をスタック*/
-	value = static_cast<uint8_t>((registerPC >> 8) & 0x00ff);
-	PhO(value);
-	value = static_cast<uint8_t>(0x00ff & registerPC);
-	PhO(value);
-	registerPC = addr;
-}
-/*サブルーチンからの帰還*/
-void Cpu::Rts(){
-	uint8_t value;
-	uint8_t temp;
-	temp = registerP;
-	/*プログラムカウンタの値を復帰*/
-	PlO(&value);
-	registerPC = static_cast<uint16_t>(value);
-	PlO(&value);
-	registerPC |= (static_cast<uint16_t>(value) << 8);
-	registerP = temp;
-	registerPC += 1;
-}
-
-/*割り込みからの復帰*/
-void Cpu::Rti(){
-	PlO(&registerP);
-	Rts();
-	registerPC -= 1;
-}
-
-/*ステータスレジスタのビットによる分岐*/
-int Cpu::Bxx(int flg, bool status){
-	bool temp;
-	uint8_t value;
-	temp = CheckBit(registerP, flg);
-	if (temp == status){
-		value = *MemoryMap(registerPC);
-		registerPC += C2S(value);
-		registerPC += 1;
-		return 1 * 3;
+	result += C2US(registerA);
+	
+	if ((result & 0x0100) == 0x0100){
+		registerF |= 0x10;
 	}
-	registerPC += 1;
-	return 0;
-}
-
-/*ソフトウェア割り込み*/
-void Cpu::Brk(uint16_t addr){
-	registerPC += 1;
-	Jsr(addr);
-	PhO(registerP);
-	StatusRegi(iFlg, true);
-	registerPC = PrgCounter(MemoryMap(addr));
-}
-
-/*スプライトデータ転送*/
-int Cpu::DMA(){
-	uint16_t addr;
-	uint8_t *ptr;
-	addr = static_cast<uint16_t>(*ioPort->ioReg[0x24]) << 8;
-	ptr = MemoryMap(addr);
-	ioPort->DMAFunc(ptr);
-	return 513 * 3;
-}
-
-
-//以下、アドレッシング
-void Cpu::Zpg(uint16_t *addr){
-	uint8_t temp;
-	temp = *MemoryMap(registerPC);
-	registerPC += 1;
-	*addr = static_cast<uint16_t>(temp);
-}
-
-
-void Cpu::ZpgO(uint8_t operand, uint16_t *addr){
-	uint16_t temp;
-	Zpg(&temp);
-	temp += static_cast<uint16_t>(operand);
-	*addr = temp & 0x00ff;
-}
-
-void Cpu::Abs(uint16_t *addr){
-	uint16_t temp;
-	Zpg(addr);
-	Zpg(&temp);
-	*addr |= (temp << 8);
-}
-
-int Cpu::AbsO(uint8_t operand, uint16_t *addr){
-	uint16_t temp;
-	Abs(&temp);
-	*addr = temp + static_cast<uint16_t>(operand);
-	if ((*addr & 0x0100) == (temp & 0x0100)){
-		return 0;
+	registerA = static_cast<char>(result);
+	
+	if (registerA == 0){
+		registerF |= 0x80; //0b1000 0000
 	}
-	return 1 * 3;
+}
+
+void Cpu::Add(char value){
+	ushort result;
+	registerF &= 0x0f;
+	registerF |= HalfCarry(registerA, value);
+	result = C2US(registerA) + C2US(value);
+	if ((result & 0x0100) == 0x0100){
+		registerF |= 0x10;
+	}
+	registerA = static_cast<char>(result);
+	
+	if (registerA == 0){
+		registerF |= 0x80; //0b1000 0000
+	}
+}
+
+void Cpu::Sbc(char value){
+	value ^= 0xff;
+	registerF ^= 0x10;
+	Adc(value);
+	registerF ^= 0x70;
 }
 	
-void Cpu::OInd(uint8_t operand, uint16_t *addr){
-	uint16_t addr1;
-	uint8_t addr2;
-	Zpg(&addr1);
-	addr1 = (addr1 + operand) & 0x00ff;
-	addr2 = *MemoryMap((addr1 + 1) & 0x00ff);
-	addr1 = static_cast<uint16_t>(*MemoryMap(addr1));
-	*addr = addr1 + (static_cast<uint16_t>(addr2) << 8);
+void Cpu::Sub(char value){
+	value ^= 0xff;
+	registerF |= 0x10;
+	Adc(value);
+	registerF ^= 0x70;
 }
 
-int Cpu::IndO(uint8_t operand, uint16_t *addr){
-	uint16_t addr1;
-	uint8_t addr2;
-	Zpg(&addr1);
-	addr2 = *MemoryMap((addr1 + 1) & 0x00ff);
-	addr1 = static_cast<uint16_t>(*MemoryMap(addr1));
-	addr1 += (static_cast<uint16_t>(addr2) << 8);
-	*addr = addr1 + static_cast<uint16_t>(operand);
-	if ((*addr & 0x0100) == (addr1 & 0x0100)){
+void Cpu::Add16(ushort *registerXY, ushort registerZW){
+	int result, value1, value2;
+	
+	registerF &= 0x80; //0b1000 0000
+	value1 = static_cast<int>(registerZW);
+	value2 = static_cast<int>(*registerXY);
+	
+	result = value1 + value2;
+	*registerXY = static_cast<ushort>(result);
+	
+	value1 = (value1 & 0x00000fff) + (value2 & 0x00000fff);
+	value1 = (value1 & 0x00001000) >> 7;
+	value1 += (result & 0x00010000) >> 12;
+	
+	registerF |= static_cast<char>(value1);
+}
+
+void Cpu::Inc(char *value){
+	registerF &= 0x1f;// 0b0001 1111
+	registerF |= HalfCarry(*value, 1);
+	*value += 1;
+	if (*value == 0){
+		registerF |= 0x80; //0b1000 0000
+	}
+}
+
+void Cpu::Dec(char *value){
+	registerF |= 0xe0; //0b1110 0000
+	registerF ^= HalfCarry(*value, 0xff);
+	*value -= 1;
+	if (*value != 0){
+		registerF &= 0x7f; //0b0111 1111
+	}
+}
+
+void Cpu::And(char value){
+	registerA &= value;
+	registerF &= 0x0f;
+	if (registerA == 0){
+		registerF |= 0xa0; //0b1010 0000
+	}else{
+		registerF |= 0x20;
+	}
+}
+
+void Cpu::Or(char value){
+	registerA |= value;
+	registerF &= 0x0f;
+	if (registerA == 0){
+		registerF |= 0x80;
+	}
+}
+
+void Cpu::Xor(char value){
+	registerA ^= value;
+	registerF &= 0x0f;
+	if (registerA == 0){
+		registerF |= 0x80;
+	}
+}
+
+void Cpu::Cp(char value){
+	char temp;
+	temp = registerA;
+	Sub(value);
+	registerA = temp;
+}
+
+void Cpu::Rlc(char *value, char carry){
+	registerF &= 0x0f;
+	registerF |= ((*value & 0x80) >> 3);
+	*value <<= 1;
+	*value |= (carry >> 4);
+}
+
+void Cpu::RlcX(char *value){
+	char carry;
+	carry = ((*value & 0x80) >> 3);
+	Rlc(value, carry);
+}
+
+void Cpu::RlX(char *value){
+	char carry;
+	carry = 0x10 & registerF;
+	Rlc(value, carry);
+}
+
+void Cpu::Rrc(char *value, char carry){
+	registerF &= 0x0f;
+	registerF |= ((*value & 0x01) << 4);
+	*value = (*value >> 1) & 0x7f;
+	*value |= (carry << 3);
+}
+
+void Cpu::RrcX(char *value){
+	char carry;
+	carry = ((*value & 0x01) << 4);
+	Rrc(value, carry);
+}
+
+void Cpu::RrX(char *value){
+	char carry;
+	carry = 0x10 & registerF;
+	Rrc(value, carry);
+}
+
+void Cpu::SraX(char *value){
+	char carry;
+	carry =  (*value & 0x80) >> 3;
+	Rrc(value, carry);
+}
+
+void Cpu::SlaX(char *value){
+	Rlc(value, 0);
+}
+
+void Cpu::SrlX(char *value){
+	Rrc(value, 0);
+}
+
+void Cpu::LdR88Imm(char *registerX, char *registerY){
+	*registerY = Imm();
+	*registerX = Imm();
+}
+
+void Cpu::LdR16Imm(ushort *registerXY){
+	char upper, lower;
+	lower = Imm();
+	upper = Imm();
+	*registerXY = Byte1to2(upper, lower);
+}
+	
+
+void Cpu ::StX16(ushort addr, ushort value){
+	*MemoryMap(addr) = static_cast<char>(0x00ff & value);
+	*MemoryMap(addr + 1) = static_cast<char>(0x00ff & (value >> 8));	
+}
+
+int Cpu::StX(ushort addr, char value){
+	if (addr < 0x8000){
+		Crg->MBC(addr, value);
+		return 0;
+		
+	}else if ((addr >= 0xa000) && (addr < 0xc000)){
+		if (Crg->ramStat == 0){
+			*MemoryMap(addr) = value;
+		}else if (Crg->ramStat < 0xff00){
+			Crg->UpdateReg();
+			sRam[0] = value;//Crg->RTCWrite(value);
+		}
+		return 0;
+		
+	}else if (addr < 0xff00){
+		*MemoryMap(addr) = value;
+		return 0;
+		
+	}else{
+		return IOReg(addr, value);
+	}
+}
+
+void Cpu::Pop88(char *registerX, char *registerY){
+	*registerY = *MemoryMap(registerSP);
+	++registerSP;
+	*registerX = *MemoryMap(registerSP);
+	++registerSP;
+}
+
+void Cpu::Push88(char registerX, char registerY){
+	--registerSP;
+	*MemoryMap(registerSP) = registerX;
+	--registerSP;
+	*MemoryMap(registerSP) = registerY;
+}
+	
+void Cpu::Push16(ushort registerXY){
+	char upper, lower;
+	upper = static_cast<char>(registerXY >> 8);
+	lower = static_cast<char>(registerXY);
+	Push88(upper, lower);
+}
+
+int Cpu::JpBranch(int flg, bool status){
+	bool regFlg;
+	regFlg = CheckBit(registerF, flg);
+	
+	if(regFlg == status){
+		Jp();
+		return 4;
+	}else{
+		registerPC += 2;
 		return 0;
 	}
-	return 1 * 3;
 }
 
-void Cpu::Ind(uint16_t *addr){
-	uint16_t addr1;
-	uint8_t addr2;
-	Abs(&addr1);
-	if ((addr1 & 0xff) == 0xff){
-		addr2 = *MemoryMap(addr1- 0xff);
+int Cpu::JrBranch(int flg, bool status){
+	bool regFlg;
+	regFlg = CheckBit(registerF, flg);
+	
+	if(regFlg == status){
+		Jr();
+		return 4;
 	}else{
-		addr2 = *MemoryMap(addr1+ 1);
+		++registerPC;
+		return 0;
 	}
-	addr1 = static_cast<uint16_t>(*MemoryMap(addr1));
-	*addr = addr1 + (static_cast<uint16_t>(addr2) << 8);
 }
 
+int Cpu::CallBranch(int flg, bool status){
+	bool regFlg;
+	regFlg = CheckBit(registerF, flg);
+	
+	if(regFlg == status){
+		Call();
+		return 12;
+	}else{
+		registerPC += 2;
+		return 0;
+	}
+}
+
+int Cpu::RetBranch(int flg, bool status){
+	bool regFlg;
+	regFlg = CheckBit(registerF, flg);
+	
+	if(regFlg == status){
+		Ret();
+		return 12;
+	}else{
+		return 0;
+	}
+}
+
+void Cpu::Swap(char *value){
+	registerF &= 0x0f;
+	*value = (*value << 4) | ((*value >> 4) & 0x0f);
+}
+	
--- src/cpumain.cpp
+++ src/cpumain.cpp
@@ -1,105 +1,389 @@
-#include <iostream>
 #include "cpu.hpp"
-#include "ioport.hpp"
-#include "mapper.hpp"
-#include "common.hpp"
+#include "cpuinline.hpp"
+#include "cartridge.hpp"
+#include <iostream>
+#include <fstream>
 
+void Boot(char *rom, ushort *registerPC, char *bank){
+	std::ifstream ifs("./cgb_boot.bin", std::ios::in | std::ios::binary);
+	if (!ifs){
+		std::cout << "ファイルが開けませんでした。" << std::endl;
+		*registerPC = 0x0100;
+		*bank = 0x01;
+		return;
+	}
 
-/*メモリマップ*/
-uint8_t *Cpu::MemoryMap(uint16_t addr){
-	uint8_t *pointer;
+	ifs.read(rom, 0x100 * sizeof(char));
+	ifs.seekg(0x200);
+	ifs.read(rom + 0x200, 0x700 * sizeof(char));
+	ifs.close();
+
+}
+
+void Cpu::BootFin(){
+	rom1 = Crg->rom;
+
+}
+
+Cpu::Cpu(Cartridge *cartridge){
 	
-	if (addr < 0x2000){
-		addr &= 0x07ff;
-		pointer = &cpuRam[addr];
+	Crg = cartridge;
+	Crg->Cpu = this;
+	rom1 = Crg->boot;
+	rom2 = &Crg->rom[romBankSize * 1];
+	sRam = Crg->ram;
+	wRam1 = new char [0x1000];
+	std::fill(wRam1, wRam1 + 0x1000, 0);
+	wRamBank = new char [0x7000];
+	std::fill(wRamBank, wRamBank + 0x7000, 0);
+	wRam2 = wRamBank;
+	vRamColor = new char [0x4000];
+	std::fill(vRamColor, vRamColor + 0x4000, 0);
 
-	}else if (addr < 0x4020){
-		ioReg = ioPort->IORead(addr);
-		pointer = &ioReg;
+	vRam = vRamColor;
+	ioReg[0x06] = 0x00;
+	ioReg[0x08] = 0xf8;
+	ioReg[0x50] = 0x00;
+	ioReg[0x40] = 0x91;
+	ioReg[0x55] = 0xff;
+		
+	registerSP = 0xfffe;
+	
+	registerPC = 0x0000;
+	Boot(rom1, &registerPC, &ioReg[0x50]);
 
-	}else if (addr < 0x6000){
-		std::cout << "ext_rom 未実装" << std::endl;
-		pointer = nullptr;
+}
+
+char *Cpu::MemoryMap(ushort addr){
+	char *ptr;
+	
+	if (addr < 0x4000){
+		ptr = &rom1[addr];
 		
 	}else if (addr < 0x8000){
-		addr -= 0x6000;
-		pointer = &mapper->prgRam[addr];//&extRam[addr];
-
-	}else{
-		//マッパーのメモリマップに従う
-		addr -= 0x8000;
+		addr -= 0x4000;
+		ptr = &rom2[addr];
 		
-		//14,15bitでcpuMapを選ぶ
-		pointer = mapper->cpuMap[addr >> 13];
-		//1～13bitで値を取得
-		pointer += addr & 0x1fff;
-	}
-	return pointer;
-}
-
-/*割り込み*/
-int Cpu::Interrupt(){
-	if (irq.irqInt == 0){
-		return 0;
-	}
-	bool flg;
-	if (irq.irqStr.nmi == true){
-		StatusRegi(bFlg, false);
-		Brk(0xfffa);
-		//irq.irqStr.nmi = false;
-		irq.irqInt = 0;
-	}else if (irq.irqStr.rst == true){
-		registerPC = PrgCounter(MemoryMap(0xfffc));
-		StatusRegi(iFlg, true);
-		irq.irqStr.rst = false;
-	}else if (irq.irqStr.irbr == true){
-		flg = CheckBit(registerP, iFlg);
-		if (flg){
-			return 0;
+	}else if (addr < 0xa000){
+		addr -= 0x8000;
+		ptr = &vRam[addr];
+		
+	}else if (addr < 0xc000){
+		if (Crg->ramStat == 0){
+			addr -= 0xa000;
+			ptr = &sRam[addr];
+		}else if (Crg->ramStat < 0xff00){
+			ptr = &sRam[0];
+		}else{
+			ptr = &Crg->rtcReg[0x0e];
 		}
 		
-		StatusRegi(bFlg, false);
-		Brk(0xfffe);
-		irq.irqStr.irbr = false;
+	}else if (addr < 0xd000){
+		addr -= 0xc000;
+		ptr = &wRam1[addr];
+		
+	}else if (addr < 0xe000){
+		addr -= 0xd000;
+		ptr = &wRam2[addr];
+		
+	}else if (addr >= 0xfe00){
+		addr -= 0xe000;
+		ptr = &etcRam[addr];
+
+	}else{
+		addr -= 0x2000;
+		ptr = MemoryMap(addr);
 	}
-	return 7 * 3;
+	return ptr;
 }
 
-/*コンストラクタ*/
-Cpu::Cpu(RomFeature *romFeature, Mapper *MAPPER, IOPort *ioP){
+int Cpu::Interrupt(){
+	if (ime){
+		char flg;
+		flg = IE & IF & 0x1f;
+		if (flg == 0){
+			return 0;
+		}
+		for (int i = 0; i < 5; ++i){
+			if ((flg & 0x01) == 0x01){
+				if (halt){
+					++registerPC;
+					halt = false;
+				}
+				Push16(registerPC);
+				registerPC = 0x0040 + (i << 3);
+				IF &= (0x01 << i) ^ 0xff;
+				ime = false;
+				return 20;
+			}else{
+				flg >>= 1;
+			}
+		}
+	}
+	return 0;
+}
+
+void Cpu::Timer(){
+	ushort temp;
 	
-	irq.irqInt = 0;
-	std::cout<<irq.irqStr.rst << std::endl;
-	irq.irqStr.rst = true;
-	ioPort = ioP;
-	ioP->cpuIRQ = &irq;
-	mapper = MAPPER;
-	mapper->cpuIrq = &irq;
-	romSize = romFeature->pRomSize;
-	registerP = 0b00100000;
-	registerS = 0xfd;
+	clockCounter += 4;
+	temp = clockCounter;
+	temp >>= timerStep[ioReg[0x07] & 0x03];
+	temp &= (static_cast<ushort>(ioReg[0x07] >> 2) & 0x01);
+	ioReg[0x04] = static_cast<char>(clockCounter >> 8);
 	
-	int i;
-	i = Interrupt();
+	if (preTimer == 1){
+		IF |= 0x04;
+		ioReg[0x05] = ioReg[0x06];
+		nextTimer = ioReg[0x05] + 1;
+		preTimer = 2;
+
+	}else if(preTimer == 2){
+		preTimer = 0;
+	}
+	
+	
+	
+	preMux &= (temp ^ 0xffff);
+	
+//	if ((temp & 0x01) == 0x01){
+	if (preMux){
+		if (ioReg[0x05] == 0xff){
+			preTimer = 1;
+		}
+		ioReg[0x05] = nextTimer;
+		++nextTimer;
+//		++ioReg[0x05];
+
+	}
+	preMux = temp;
+	
 	
 }
 
-/*メインのシーケンス*/
-int Cpu::mainRun(){
-	
+int Cpu::Sequence(){
 	int counter;
-	/*オペコードを取得*/
-	uint8_t opeCode;
-	opeCode = *MemoryMap(registerPC++);
+	int i = 0;
+	ushort opeCode;
+	counter = Interrupt();
 
-	/*メインの処理実行*/
-	counter = (this->*cpuInstrs[opeCode])();
+	while (i < counter){
+		Timer();
+		i += 4;
+	}
+
+	opeCode = C2US(Imm());
+
+	counter += (this->*cpuInstrs[opeCode])();
+	counter += cpuCycle[opeCode];
+
+	while (i < counter){
+		Timer();
+		i += 4;
+	}
+	return counter >> doubleSpeed;
+}
+
+int Cpu::IOReg(ushort addr, char value){
+	addr &= 0x00ff;
+	int counter = 0;
 	
-	counter += cpuCycles[opeCode];
-	StatusRegi(rFlg, true);
-	counter += Interrupt();
-	/*処理にかかったクロック数を返す*/
+	if (addr == 0x00){
+		if ((value & 0x30) == 0x20){
+			ioReg[0x00] = key[0];
+			ioReg[0x00] |= 0xe0;
+		}else if ((value & 0x30) == 0x10){
+			ioReg[0x00] = key[1];
+			ioReg[0x00] |= 0xd0;
+		}else if ((value & 0x30) == 0x30){
+			ioReg[0x00] = 0xff;
+		}
+		
+	}else if (addr == 0x04){
+		clockCounter = 0xfffc;
+		
+	}else if (addr == 0x05){
+		if (preTimer == 0){
+			ioReg[0x05] = value;
+			nextTimer = value + 1;
+		}else if (preTimer == 1){	
+			preTimer = 0;
+			ioReg[0x05] = value;
+			nextTimer = value + 1;
+		}
+		
+	}else if (addr == 0x06){
+		nextTimer = value;
+		ioReg[0x06] = value;
+		
+	}else if (addr == 0x41){
+		ioReg[0x41] &= 0x07;
+		ioReg[0x41] |= (value | 0x80) & 0xf8;
+		
+	}else if (addr == 0x46){
+		char *dma;
+		ioReg[0x46] = value;
+		addr = static_cast<ushort>(value) << 8;
+		dma = MemoryMap(addr);
+		for (int i = 0; i < 0xa0; ++i){
+			oam[i] = dma[i];
+		}
+		
+	}else if (addr == 0x4d){
+		ioReg[0x4d] &= 0x80;
+		ioReg[0x4d] |= value & 0x01;
+		
+	}else if (addr == 0x4f){
+		value |= 0xfe;
+		ioReg[0x4f] = value;
+		if (value == 0xfe){
+			vRam = vRamColor;
+		}else{
+			vRam = vRamColor + 0x2000;
+		}
+		
+	}else if (addr == 0x50){
+		ioReg[0x50] = value;
+		if (!CheckBit(rom1[0x143], 7)){
+			**palette[0] = 0x01;
+			*(*palette[0] + 1) = 0xf0;
+			*palette[0] = &ioReg[0x47];
+			*palette[1] = &ioReg[0x48];
+		}
+		BootFin();
+		
+	}else if (addr == 0x51){
+		hdmaS &= 0x00ff;
+		hdmaS |= C2US(value) << 8;
+		
+	}else if (addr == 0x52){
+		hdmaS &= 0xff00;
+		hdmaS |= static_cast<ushort>(value) & 0x00f0;
+		
+	}else if (addr == 0x53){
+		hdmaT &= 0x80ff;
+		hdmaT |= static_cast<ushort>(value & 0x1f) << 8;
+		
+	}else if (addr == 0x54){
+		hdmaT &= 0xff00;
+		hdmaT |= static_cast<ushort>(value) & 0x00f0;
+		
+	}else if (addr == 0x55){
+		if (CheckBit(ioReg[0x55], 7)){
+			if (!CheckBit(value, 7)){
+				addr = (C2US(value) + 1) << 4;
+				for (int i = 0; i < addr; ++i){
+					*MemoryMap(hdmaT++) = *MemoryMap(hdmaS++);
+				}
+				ioReg[0x55] = 0xff;
+				counter = (addr << 1) << doubleSpeed;
+			
+			}else{
+				ioReg[0x55] = value & 0x7f;
+			}
+		}else{
+			if (!CheckBit(value, 7)){
+				ioReg[0x55] |= 0x80;
+			}
+		}
+
+	}else if (addr == 0x68){
+		ioReg[0x68] = value & 0xbf;
+		addr = static_cast<ushort>(value) & 0x3f;
+		ioReg[0x69] = bgPalette[addr];
+		
+	}else if (addr == 0x69){
+		Palette(value, static_cast<ushort>(ioReg[0x68] & 0x3f) | 0x40, bgPalette);
+		if (CheckBit(ioReg[0x68], 7)){
+			++ioReg[0x68];
+			ioReg[0x68] &= 0xbf;
+		}
+		ioReg[0x69] = bgPalette[C2US(ioReg[0x68])];
+		
+	}else if (addr == 0x6a){
+		ioReg[0x6a] = value & 0xbf;
+		addr = static_cast<ushort>(value) & 0x3f;
+		ioReg[0x6b] = spritePalette[addr];
+		
+	}else if (addr == 0x6b){
+		Palette(value, static_cast<ushort>(ioReg[0x6a] & 0x3f), spritePalette);
+		if (CheckBit(ioReg[0x6a], 7)){
+			++ioReg[0x6a];
+			ioReg[0x6a] &= 0xbf;
+		}
+		ioReg[0x6b] = spritePalette[C2US(ioReg[0x6a])];
+		
+	}else if (addr == 0x70){
+		addr = static_cast<ushort>(value) & 0x07;
+		ioReg[0x70] = 0xf8 | value;
+		
+		if (addr != 0){
+			--addr;
+		}
+		addr <<= 12;
+		wRam2 = &wRamBank[addr];
+		
+	}else{
+		ioReg[addr] = value;
+	}
+	return counter;
+
+}
+
+int Cpu::HDMA(){
+	int i, counter;
+	if (CheckBit(ioReg[0x55], 7)){
+		return 0;
+	}
+	
+	counter = Interrupt();
+	counter >>= doubleSpeed;
+
+	for (i = 0; i < 0x10; ++i){
+		*MemoryMap(hdmaT++) = *MemoryMap(hdmaS++);
+	}
+	if (ioReg[0x55] == 0){
+		ioReg[0x55] = 0xff;
+	}else{
+		--ioReg[0x55];
+	}
+	counter += 32;
+	
+	i = counter;
+	while(i > 0){
+		Timer();
+		i -= 2;
+	}
+	
 	return counter;
 }
 
+void Cpu::Palette(char value, ushort addr, char *palette){
+	char temp = value;
+	palette[addr & 0x3f] = value;
+	if (!CheckBit(addr, 0)){
+		addr >>= 1;
+		value &= 0x1f;
+		color[addr].color[2] = (value << 3) | (value >> 2);
+		value = (temp >> 5) & 0x07;
+		color[addr].color[1] &= 0xc6;
+		color[addr].color[1] |= (value << 3) | (value >> 2);
 
+	}else{
+		addr >>= 1;
+		value &= 0x03;
+		color[addr].color[1] &= 0x39;
+		color[addr].color[1] |= (value << 6) | (value << 1);
+		value = temp & 0x7c;
+		color[addr].color[0] = (value << 1) | (value >> 4);
+
+	}
+}
+	
+
+Cpu::~Cpu(){
+	
+	delete[] wRam1;
+	delete[] wRamBank;
+	delete[] vRamColor;
+}
